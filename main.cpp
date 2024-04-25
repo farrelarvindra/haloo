613c9a29cf84f4535966c4cf946d087f79818d8a
farrelarvindra <167633851+farrelarvindra@users.noreply.github.com>
2024-04-25 22:25:45 +0700

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#define ll long long
+using namespace std;
+
+int main() {
+  ll n;
+  cin >> n;
+
+  pair<ll, ll> j[100005];
+  for (int i = 0; i < n; i++) {
+    cin >> j[i].first >> j[i].second;
+  }
+  sort(j, j + n);
+  for (int i = 0; i < n; i++) {
+  }
+}
